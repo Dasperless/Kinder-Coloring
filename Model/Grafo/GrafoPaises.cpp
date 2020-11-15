6a474cb9e90fc28345afe60520a77cc7d5e134bb
Darío Vargas <dariovm13@gmail.com>
2020-11-14 20:57:34 -0600

    Cambios menores en GrafoPais

--- Model/Grafo/GrafoPaises.cpp
+++ Model/Grafo/GrafoPaises.cpp
@@ -7,7 +7,18 @@
  * @brief Construye un objeto de tipo GrafoPaises
  * 
  */
-GrafoPaises::GrafoPaises() {}
+GrafoPaises::GrafoPaises() {
+	int indiceX = 0;
+	int indiceY = 0;
+	for(int i = 0; i < 4; i++){
+		int tempIndiceX = indiceX;
+		int tempIndiceY = indiceX;
+		indiceX += 500;
+		indiceY += 250;
+		Bucket *nuevoBucket = new Bucket(tempIndiceX, indiceX, tempIndiceY,indiceY);
+		bucketPaisesVecinos.push_back(nuevoBucket);
+	}
+}
 
 /**
  * 
@@ -23,6 +34,7 @@ GrafoPaises::GrafoPaises() {}
  */
 	void GrafoPaises::insertaNodo(string pIdPais,string pColorPais,string pCoordenadas){
 		NodoPais *nodoInsertar = new NodoPais(pIdPais, pColorPais, pCoordenadas);
+		insertarNodoBucket(nodoInsertar);
 		listaPaises.push_back(nodoInsertar);
 	}
 
@@ -34,3 +46,23 @@ GrafoPaises::GrafoPaises() {}
 	vector<NodoPais *> GrafoPaises::obtenerListaNodos(){
 		return listaPaises;
 	}
+
+/**
+ * @brief Inserta en el bucket correspondiente un pais.
+ * 
+ */
+	void GrafoPaises::insertarNodoBucket(NodoPais *pNodoPais){
+		int tamVectorBucket = bucketPaisesVecinos.size();
+		int maxCoordXPais = pNodoPais->getMaxCoordX();
+		int minCoordXPais = pNodoPais->getMinCoordX();
+		int maxCoordYPais = pNodoPais->getMaxCoordY();
+		int minCoordYPais = pNodoPais->getMinCoordY();
+
+		for(int indiceBucket = 0; indiceBucket < tamVectorBucket; indiceBucket++){
+			Bucket *bucketActual = bucketPaisesVecinos.at(indiceBucket);
+			if(bucketActual->isInRange(maxCoordXPais,minCoordXPais) && bucketActual->isInRange(maxCoordYPais,minCoordYPais)){
+				bucketActual->insertarPais(pNodoPais);
+			}
+		}
+	}
+
